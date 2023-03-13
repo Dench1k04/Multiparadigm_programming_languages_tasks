36850eff497de75092024847641bbef6aec8bf41
Denis Dolgov <denis.d@ksyste.ms>
2023-03-13 22:28:09 +0200

    Task 1

---
+++ Dolgov_Task1.py
@@ -0,0 +1,8 @@
+print("Introduction to programming, Lab № 1")
+print("Dolgov Denis IKM-221a")
+
+print(("Vladislav" + ", ") * 45 + "Vladislav")
+
+print(7.8 - (1+(2.1/3))/(78+21.3))
+
+print("Hello")
