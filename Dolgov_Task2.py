94726f17ba726c0308b62a80225e603e36f05b41
Denis Dolgov <denis.d@ksyste.ms>
2023-03-27 09:07:07 +0300

    delete files

--- Dolgov_Task2.py
+++
@@ -1,9 +0,0 @@
-print("Introduction to programming, Lab № 2")
-print("Dolgov Denis IKM-221a")
-
-x = int(input("Enter x"))
-y = int(input("Enter y"))
-z = int(input("Enter z"))
-
-a = x-(x+(y/z))/(78+y)
-print("Result is: ", a)
