ff061d90dabda8f736b4bd8f8bcc1b7a0df7a73e
Denis Dolgov <denis.d@ksyste.ms>
2023-04-13 20:16:28 +0300

    Task_2

---
+++ dolgov_task2.py
@@ -0,0 +1,10 @@
+print("Introduction to programming, Lab № 2")
+print("Dolgov Denis IKM-221a")
+
+TEMPLATE = "Enter {}"
+x = int(input(TEMPLATE.format('x')))
+y = int(input("Enter y"))
+z = int(input("Enter z"))
+
+a = x-(x+(y/z))/(78+y)
+print("Result is: ", a)
