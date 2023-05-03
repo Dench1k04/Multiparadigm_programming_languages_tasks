4856559650fda7e54a91ac4ebd9055e72b9bf5b1
Denis <118078020+Dench1k04@users.noreply.github.com>
2023-05-03 13:06:31 +0300

    Add files via upload

---
+++ Dolgov_Task2.py
@@ -0,0 +1,9 @@
+print("Introduction to programming, Lab № 2")
+print("Dolgov Denis IKM-221a")
+
+x = int(input("Enter x"))
+y = int(input("Enter y"))
+z = int(input("Enter z"))
+
+a = x-(x+(y/z))/(78+y)
+print("Result is: ", a)
