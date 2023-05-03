de58c6280c1826fc4b31a8a5c2c30f3ca41bbcf7
Denis Dolgov <denis.d@ksyste.ms>
2023-05-03 11:19:02 +0300

    Task_4

--- dolgov_task4.py
+++ dolgov_task4.py
@@ -4,7 +4,6 @@
 print('Multiparadigm programming languages, Lab № 4')
 print(' Dolgov Denis IKM-221a')
 
-
 # We ask the user to enter the number N
 n = int(input('Enter your N: '))
 
@@ -13,7 +12,7 @@ for num in range(2, n):
     # We check whether the number is prime
     is_prime = True
     for i in range(2, num):
-        if num  %i == 0:
+        if num % i == 0:
             is_prime = False
             break
     # If the number is simple, display it on the screen
